62c08e27eab5bca5e6865ce43d20e9c3283f6da4
Ananta Pandu Wicaksana <anpandumail@gmail.com>
2026-05-23 23:36:25 +0700

    modules + melbourne + california

--- notebooks/modules/utils.py
+++ notebooks/modules/utils.py
@@ -9,3 +9,15 @@ def xfrange(start, stop, step):
         yield start + i * step
         i += 1
 
+def scale(input_df, cols=None, scaler_class=None, scalers=None):
+    _input_df = input_df.copy()
+    if scalers:
+        for k, v in scalers.items():
+            _input_df[k] = v.transform(_input_df[[k]].copy())
+    else:
+        scalers = {}
+        for col in cols:
+            scaler = scaler_class()
+            _input_df[col] = scaler.fit_transform(_input_df[[col]].copy())
+            scalers[col] = scaler
+    return _input_df, scalers
